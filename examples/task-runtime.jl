d7f875e1c235195c9e1f0d443a4cf243bcaf2f71
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2022-01-22 13:18:14 -0600

    Update task RT demo for perf_event_output

--- examples/task-runtime.jl
+++ examples/task-runtime.jl
@@ -1,152 +1,216 @@
+using CEnum
+using CBinding
+
+module LibJulia
+    using System
+    @sys using libc.POSIX
+    using CBinding
+
+    let
+        JULIA_DIR=Base.dirname(Base.dirname(Base.julia_cmd().exec[1]))
+        println("Reading from $JULIA_DIR/include/julia")
+        c`-I $(JULIA_DIR)/include/julia -L$(JULIA_DIR)/lib -L$(JULIA_DIR)/lib/julia -ljulia-internal`
+    end
+    c"""
+        #undef _Atomic
+        #define _Atomic(x) x
+        #include <julia.h>
+    """ij
+
+end
+
+# Make sure these are defined
+LibJulia.jl_ptls_t
+LibJulia.jl_task_t
+
 using BPFnative; import BPFnative: RT, API, Host
 
 #=
 This example implements a monitor for some of the USDT probes added in
 https://github.com/JuliaLang/julia/pull/43453. It monitors task creation,
-running, and pausing for the current process.
+running, pausing, UV loop processing, and sleep/wake transitions for the
+current process' threads. It also tracks GC activity, including start/end, mark
+and sweep, stop-the-world, and finalizer execution.
 =#
 
-const MAXTASKS = 128
-const child_parent_map = RT.RTMap(;name="child_parent_map", maptype=API.BPF_MAP_TYPE_HASH, keytype=UInt64, valuetype=UInt64, maxentries=MAXTASKS)
-const run_map = RT.RTMap(;name="run_map", maptype=API.BPF_MAP_TYPE_HASH, keytype=UInt64, valuetype=Int64, maxentries=MAXTASKS)
-const ptls_map = RT.RTMap(;name="ptls_map", maptype=API.BPF_MAP_TYPE_HASH, keytype=UInt64, valuetype=UInt64, maxentries=MAXTASKS)
-const state_map = RT.RTMap(;name="state_map", maptype=API.BPF_MAP_TYPE_HASH, keytype=UInt64, valuetype=Int64, maxentries=MAXTASKS)
-const process_events_map = RT.RTMap(;name="process_events_map", maptype=API.BPF_MAP_TYPE_ARRAY, keytype=Int32, valuetype=UInt64, maxentries=1)
-const gc_state_map = RT.RTMap(;name="gc_state_map", maptype=API.BPF_MAP_TYPE_ARRAY, keytype=Int32, valuetype=Int32, maxentries=1)
+const MAXTASKS = 1024
+const rb_map = RT.RTMap(;name="rb_map", maptype=API.BPF_MAP_TYPE_PERF_EVENT_ARRAY, keytype=Cint, valuetype=UInt32, maxentries=MAXTASKS)
+@cenum RuntimeEventKind begin
+    TaskCreated
+    TaskRunning
+    TaskPaused
+    TaskUVLoop
+    GCRunning
+    GCStopped
+    GCWorldStopping
+    GCMarking
+    GCSweeping
+    GCFinalizer
+    ThreadWake
+    ThreadWakeup
+    ThreadSleep
+    ThreadTaskQueueWake
+    ThreadTaskWake
+    ThreadUVWake
+end
+struct RuntimeEvent
+    kind::RuntimeEventKind
+    timestamp::UInt64
+    arg1::UInt64
+    arg2::UInt64
+end
+
+function submit_event!(ctx, kind::RuntimeEventKind, arg1=0, arg2=0)
+    event_buf = RT.create_buffer(sizeof(RuntimeEvent))
+    ts = RT.ktime_get_ns()
+    event = RuntimeEvent(kind, ts, arg1, arg2)
+    unsafe_store!(reinterpret(Ptr{RuntimeEvent}, pointer(event_buf)), event)
+    RT.perf_event_output(ctx, rb_map, 0, event_buf)
+end
 
 new_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", "rt__new__task"; license="GPL") do ctx
     parent = something(API.get_param(ctx, Val(1)), UInt64(0))
     child = something(API.get_param(ctx, Val(2)), UInt64(0))
-    ptls = something(API.get_param(ctx, Val(3)), UInt64(0))
-    child_parent_map[child] = parent
-    run_map[child] = 0
-    ptls_map[child] = ptls
-    state_map[child] = 0
+    submit_event!(ctx, TaskCreated, parent, child)
     0
 end
 API.load(new_task)
 run_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", "rt__run__task"; license="GPL", merge_with=[new_task]) do ctx
     task = something(API.get_param(ctx, Val(1)), UInt64(0))
-    ptls = something(API.get_param(ctx, Val(2)), UInt64(0))
-    old_count = something(run_map[task], 0)
-    run_map[task] = old_count + 1
-    ptls_map[task] = ptls
-    state_map[task] = 1
+    submit_event!(ctx, TaskRunning, task)
     0
 end
-@assert run_task isa BPFnative.ProbeSet
-println(run_task)
 API.load(run_task)
 pause_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", "rt__pause__task"; license="GPL", merge_with=[new_task]) do ctx
     task = reinterpret(UInt64, API.get_param(ctx, Val(1)))
-    ptls = reinterpret(UInt64, API.get_param(ctx, Val(2)))
-    ptls_map[task] = ptls
-    state_map[task] = 2
+    submit_event!(ctx, TaskPaused, task)
     0
 end
 API.load(pause_task)
 start_process_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", "rt__start__process__events"; license="GPL", merge_with=[new_task]) do ctx
     task = something(API.get_param(ctx, Val(1)), UInt64(0))
-    process_events_map[1] = task
-    state_map[task] = 3
+    submit_event!(ctx, TaskUVLoop, task)
     0
 end
 API.load(start_process_task)
 finish_process_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", "rt__finish__process__events"; license="GPL", merge_with=[new_task]) do ctx
     task = something(API.get_param(ctx, Val(1)), UInt64(0))
-    state_map[task] = 1
+    submit_event!(ctx, TaskRunning, task)
     0
 end
 API.load(finish_process_task)
 
-#= TODO
-multiq_state_task = new_task
-for (probe, multiq_state) in [
-    ("rt__multiq__insert__success", 1),
-    ]
-@eval multiq_state_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", $probe; license="GPL", merge_with=[multiq_state_task]) do ctx
-    multiq_state_map[1] = $multiq_state
-    0
-end
-API.load(multiq_state_task)
-end
-=#
-
 gc_state_task = new_task
 for (probe, gc_state) in [
-    ("gc__begin", 1),
-    ("gc__stop_the_world", 2),
-    ("gc__mark__begin", 3),
-    ("gc__mark__end", 1),
-    ("gc__sweep__begin", 4),
-    ("gc__sweep__end", 1),
-    ("gc__end", 0),
-    ("gc__finalizer", 5)]
+    ("gc__begin", GCWorldStopping),
+    ("gc__stop_the_world", GCRunning),
+    ("gc__mark__begin", GCMarking),
+    ("gc__mark__end", GCRunning),
+    ("gc__sweep__begin", GCSweeping),
+    ("gc__sweep__end", GCRunning),
+    ("gc__end", GCStopped),
+    ("gc__finalizer", GCFinalizer)]
     # TODO: Finalizer end?
 @eval gc_state_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", $probe; license="GPL", merge_with=[gc_state_task]) do ctx
-    gc_state_map[1] = $gc_state
+    submit_event!(ctx, $gc_state)
     0
 end
 API.load(gc_state_task)
 end
 
-host_child_parent_map = Host.hostmap(new_task, child_parent_map)
-host_run_map = Host.hostmap(new_task, run_map)
-host_ptls_map = Host.hostmap(new_task, ptls_map)
-host_state_map = Host.hostmap(new_task, state_map)
-host_process_events_map = Host.hostmap(start_process_task, process_events_map)
-host_gc_state_map = Host.hostmap(gc_state_task, gc_state_map)
-
-f() = Libc.systemsleep(0.01)
-@sync for i in 1:100
-    Threads.@spawn f()
+thread_state_task = new_task
+for (probe, thread_state) in [
+    ("rt__sleep__check__wake", ThreadWake), # TODO: Expose old_state arg
+    ("rt__sleep__check__wakeup", ThreadWakeup),
+    ("rt__sleep__check__sleep", ThreadSleep),
+    ("rt__sleep__check__taskq__wake", ThreadTaskQueueWake),
+    ("rt__sleep__check__task__wake", ThreadTaskWake),
+    ("rt__sleep__check__uv__wake", ThreadUVWake),
+    ]
+@eval thread_state_task = USDT(getpid(), Base.julia_cmd().exec[1], "julia", $probe; license="GPL", merge_with=[thread_state_task]) do ctx
+    ptls = something(API.get_param(ctx, Val(1)), UInt64(0))
+    submit_event!(ctx, $thread_state, ptls)
+    0
+end
+API.load(thread_state_task)
 end
 
-for task in keys(host_run_map)
-    parent = try
-        repr(host_child_parent_map[task])
-    catch
-        "???"
-    end
-    t = try
-        repr(host_run_map[task])
-    catch
-        "???"
-    end
-    ptls = try
-        repr(host_ptls_map[task])
-    catch
-        "???"
-    end
-    state = try
-        x = host_state_map[task]
-        if x == 0
-            "created"
-        elseif x == 1
-            "running"
-        elseif x == 2
-            "paused"
-        elseif x == 3
-            "processing"
-        end
-    catch
-        "???"
-    end
-    println("Task $task (parent $parent) (PTLS $ptls) (state $state) ran $t times")
-end
-ptask = host_process_events_map[1]
-println("Last task processing events: $ptask")
+const ptls_to_tid = Dict{UInt64,Int}()
 
-# TODO: This technically should not work, since tasks are paused during GC
-gc_state = host_gc_state_map[1]
-if gc_state == 1
-    println("GC: Running")
-elseif gc_state == 2
-    println("GC: Stopping the world")
-elseif gc_state == 3
-    println("GC: Marking")
-elseif gc_state == 4
-    println("GC: Sweeping")
-elseif gc_state == 5
-    println("GC: Running finalizers")
+function sample_cb(ctx::Ptr{Dict{UInt64,Int}}, cpu::Cint, data::Ptr{RuntimeEvent}, size::UInt32)
+    ptls_map = unsafe_load(ctx)
+    event = unsafe_load(data)
+    kind = event.kind
+
+    Core.print("[$(event.timestamp)] (CPU $cpu) ")
+    if kind == TaskCreated
+        parent = event.arg1
+        child = event.arg2
+        tid = reinterpret(Cptr{LibJulia.jl_task_t}, parent).tid[]
+        Core.println("(TID $tid) Task Created (parent $parent, child $child)")
+    elseif kind == TaskRunning
+        task = event.arg1
+        tid = reinterpret(Cptr{LibJulia.jl_task_t}, task).tid[]
+        Core.println("(TID $tid) Task Running (task $task)")
+    elseif kind == TaskPaused
+        task = event.arg1
+        tid = reinterpret(Cptr{LibJulia.jl_task_t}, task).tid[]
+        Core.println("(TID $tid) Task Paused (task $task)")
+    elseif kind == TaskUVLoop
+        task = event.arg1
+        tid = reinterpret(Cptr{LibJulia.jl_task_t}, task).tid[]
+        Core.println("(TID $tid) Task Running UV loop (task $task)")
+    elseif kind == GCStopped
+        Core.println("GC: Done")
+    elseif kind == GCRunning
+        Core.println("GC: Running")
+    elseif kind == GCWorldStopping
+        Core.println("GC: Stopping the world")
+    elseif kind == GCMarking
+        Core.println("GC: Marking")
+    elseif kind == GCSweeping
+        Core.println("GC: Sweeping")
+    elseif kind == GCFinalizer
+        Core.println("GC: Running finalizers")
+    elseif kind == ThreadWake
+        ptls = event.arg1
+        tid = reinterpret(LibJulia.jl_ptls_t, ptls).tid[]
+        Core.println("(TID $tid) Thread Wake")
+    elseif kind == ThreadWakeup
+        ptls = event.arg1
+        tid = reinterpret(LibJulia.jl_ptls_t, ptls).tid[]
+        Core.println("(TID $tid) Thread Wakeup")
+    elseif kind == ThreadSleep
+        ptls = event.arg1
+        tid = reinterpret(LibJulia.jl_ptls_t, ptls).tid[]
+        Core.println("(TID $tid) Thread Sleep")
+    elseif kind == ThreadTaskQueueWake
+        ptls = event.arg1
+        tid = reinterpret(LibJulia.jl_ptls_t, ptls).tid[]
+        Core.println("(TID $tid) Thread Task Queue Wake")
+    elseif kind == ThreadTaskWake
+        ptls = event.arg1
+        tid = reinterpret(LibJulia.jl_ptls_t, ptls).tid[]
+        Core.println("(TID $tid) Thread Task Wake")
+    elseif kind == ThreadUVWake
+        ptls = event.arg1
+        tid = reinterpret(LibJulia.jl_ptls_t, ptls).tid[]
+        Core.println("(TID $tid) Thread UV Wake")
+    end
+
+    return nothing
+end
+sample_cb_ptr = @cfunction(sample_cb, Cvoid, (Ptr{typeof(ptls_to_tid)}, Cint, Ptr{RuntimeEvent}, UInt32))
+function lost_cb(ctx::Ptr, cpu::Cint, lost::UInt32)
+    Core.println("(CPU $cpu) LOST $lost ENTRIES")
+    ccall(:abort, Cvoid, ())
+    nothing
+end
+lost_cb_ptr = @cfunction(lost_cb, Cvoid, (Ptr{typeof(ptls_to_tid)}, Cint, UInt32))
+host_rb_map = Host.PerfBuffer{Cint,UInt32}(API.fd(API.findmap(new_task, "rb_map")), 32; sample_cb=sample_cb_ptr, lost_cb=lost_cb_ptr, ctx=Base.pointer_from_objref(ptls_to_tid))
+
+while true
+    ts = [Threads.@spawn sleep(sum(rand(1000))/1000) for i in 1:10]
+    Host.poll(host_rb_map, 100)
+    wait.(ts)
 end
